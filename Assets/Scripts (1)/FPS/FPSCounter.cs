7bc4bf07cd46cfafc2806c90103bae8c384d123f
Orange13378 <muslim_askerli@mail.ru>
2024-06-07 14:10:04 +0300

    Add FPS checker

---
+++ Assets/Scripts (1)/FPS/FPSCounter.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace CubeECS
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        private int _frameCount;
+        private float _elapsedTime;
+        private float _updateInterval;
+        private float _fps;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 300;
+
+            QualitySettings.vSyncCount = 0;
+        }
+
+        private void Update()
+        {
+            _frameCount++;
+            _elapsedTime += Time.deltaTime;
+
+            if (_elapsedTime >= _updateInterval)
+            {
+                _fps = _frameCount / _elapsedTime;
+                _frameCount = 0;
+                _elapsedTime = 0f;
+            }
+        }
+
+        public float GetFPS()
+        {
+            return _fps;
+        }
+    }
+}
